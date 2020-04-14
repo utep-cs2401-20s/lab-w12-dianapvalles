309460f46f3aea9d2845720dcc18e5db0f3fbb06
dianapvalles <dianapaolavalles@gmail.com>
2020-04-13 19:54:23 -0600

    Implemented constructors, insert and size methods, created tester class

--- src/myBinarySearchTreeNode.java
+++ src/myBinarySearchTreeNode.java
@@ -5,12 +5,20 @@ class myBinarySearchTreeNode{
     
   myBinarySearchTreeNode(int inValue){
     // created a new node with empty child pointers
+    myValue = inValue;
+    left = null;
+    right = null;
 
   }
   
   myBinarySearchTreeNode(int[] A){
     // creates a new Binary Search Tree rooted at the first value in the array
     /// by inserting elements into the tree in the order they are given in A.
+    myBinarySearchTreeNode tree = new myBinarySearchTreeNode(A[0]);
+
+    for(int i = 0; i < A.length; i++){
+      tree.insert(A[i]);
+    }
 
   }
   
@@ -21,8 +29,28 @@ class myBinarySearchTreeNode{
     //    * as the right child, 
     //    * in the left subtree,
     //    * or in the right subtree.
-    // If the value already exists in the tree, no action is taken. 
-    
+    // If the value already exists in the tree, no action is taken.
+    if(inValue > this.myValue){
+      if(right == null){
+        right = new myBinarySearchTreeNode(inValue);
+      }
+      else{
+        right.insert(inValue); //recursive call
+      }
+    }
+    else if(inValue < this.myValue){
+      if(left == null){
+        left = new myBinarySearchTreeNode(inValue);
+      }
+      else{
+        left.insert(inValue); //recursive call
+      }
+    }
+    if(inValue == this.myValue){
+      // do not insert duplicates
+      System.out.println("ERROR: duplicated value");
+    }
+
   }
   
   public int height(){
@@ -39,7 +67,21 @@ class myBinarySearchTreeNode{
      // (i.e. the number of the recursie calls).
     return -1;
   }
-  
+
+  public int size(){
+    // This method recursively calculates the number of nodes in the (sub)tree.
+    int sum = 1;
+
+    if(left != null){
+      sum += left.size();
+    }
+    if(right != null){
+      sum += right.size();
+    }
+
+    return sum;
+  }
+
   // Utility function included so you can debug your solution. 
   public void print() { print(""); }
   private void print(String prefix) {
