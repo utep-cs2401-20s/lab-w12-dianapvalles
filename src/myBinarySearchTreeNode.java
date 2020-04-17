7784c61513a1b56247b61b1727e9028b6ef1013a
dianapvalles <dianapaolavalles@gmail.com>
2020-04-16 18:23:13 -0600

    Finished method and tests

--- src/myBinarySearchTreeNode.java
+++ src/myBinarySearchTreeNode.java
@@ -28,9 +28,11 @@ class myBinarySearchTreeNode{
     //    * in the left subtree,
     //    * or in the right subtree.
     // If the value already exists in the tree, no action is taken.
+
+    //compares the values to find the correct position
     if(inValue > this.myValue){
       if(right == null){
-        right = new myBinarySearchTreeNode(inValue);
+        right = new myBinarySearchTreeNode(inValue); //inserts the node -- correct position
       }
       else{
         right.insert(inValue); //recursive call
@@ -38,7 +40,7 @@ class myBinarySearchTreeNode{
     }
     else if(inValue < this.myValue){
       if(left == null){
-        left = new myBinarySearchTreeNode(inValue);
+        left = new myBinarySearchTreeNode(inValue); //inserts the node -- correct position
       }
       else{
         left.insert(inValue); //recursive call
@@ -46,7 +48,7 @@ class myBinarySearchTreeNode{
     }
     if(inValue == this.myValue){
       // do not insert duplicates
-      System.out.println("ERROR: duplicated value"); //Error message
+      System.out.println("Couldn't create node, duplicated value"); //Error message
     }
   }
   
@@ -54,17 +56,17 @@ class myBinarySearchTreeNode{
      // This method recursively calculates the height of the entire (sub)tree.
      // This method will take O(n) time
 
-    int leftH = 0;
-    int rightH = 0;
+    int leftH = 0; // value that will hold the left subtree's number of edges
+    int rightH = 0; // value that will hold the right subtree's number of edges
 
     if(left != null){
-      leftH = left.height() + 1;
+      leftH = left.height() + 1; //adds one to the height of the left side
     }
     if(right != null){
-      rightH = right.height() + 1;
+      rightH = right.height() + 1; //adds one to the height of the right side
     }
 
-    return (leftH > rightH) ? (leftH) : (rightH);
+    return (leftH > rightH) ? (leftH) : (rightH); //compares which of the subtrees has a greater height
   }
   
   public int depth(int search){
@@ -74,34 +76,44 @@ class myBinarySearchTreeNode{
      // Additionally, remember that the depth is the number of nodes on the path from a node to the root 
      // (i.e. the number of the recursive calls).
 
-    if (search < myValue) {
+    if (search < myValue) { //looks for the possible position of the node by comparing it to the root first
       if (left != null) {
-        return 1 + left.depth(search);
+        int leftS = left.depth(search);
+
+        if(leftS == -1) // checks if the returned value was -1 so that it keeps returning -1
+          return -1;
+
+        return 1 + left.depth(search); // if the returned value was not -1 then it keeps counting the edges
       }
     }
 
     if (search > myValue) {
       if (right != null) {
-        return 1 + right.depth(search);
+        int rightS = right.depth(search);
+
+        if(rightS == -1) // checks if the returned value was -1 so that it keeps returning -1
+          return -1;
+
+        return 1 + right.depth(search); // if the returned value was not -1 then it keeps counting the edges
       }
-      return -1;
     }
 
-    if(search == myValue)
+    if(search == myValue) // the value was found then it returns 0
       return 0;
 
-    return -1;
+    return -1; // the pointers reached null and no value was found
   }
 
   public int size(){
     // This method recursively calculates the number of nodes in the (sub)tree.
+    // Iterates over the binary tree to return the size
     int sum = 1;
 
     if(left != null){
-      sum += left.size();
+      sum += left.size(); // increments the sum by 1
     }
     if(right != null){
-      sum += right.size();
+      sum += right.size(); //increments the sum by 1
     }
 
     return sum;
--- src/myBinarySearchTreeTester.java
+++ src/myBinarySearchTreeTester.java
@@ -4,68 +4,14 @@ import static org.junit.jupiter.api.Assertions.*;
 public class myBinarySearchTreeTester {
 
     /*
-        * The purpose of this test is to check the correct implementation of the method height
-        * This method calculates the height of the entire subtree and returns the greatest one (height of the entire tree)
-        * If the test fails it means that something went wrong while creating the binary tree or there must be an error due to counting in the height
-        * The root is assigned with 2
-        * TEST: PASSED
-    */
+         * The purpose of this test is to check the correct implementation of the method insert
+         * This method checks if the insertion method worked (creating each node where it is supposed to, not creating duplicates)
+         * If it fails, the method could be adding duplicated values or putting the values in the wrong position
+         * The root is assigned with 2
+         * TEST: PASSED
+     */
     @Test
     public void tree1(){
-        int[] futureTree = {2,4,1,3,6,7,5};
-        myBinarySearchTreeNode tree = new myBinarySearchTreeNode(futureTree);
-
-        tree.print();
-
-        assertEquals(3, tree.height());
-    }
-
-    /*
-     * The purpose of this test is to check the correct implementation of the method size
-     * The array passed contains a duplicated value (it should not be part of the tree)
-     * If the test
-     * It is supposed to calculate the number of nodes in the binary search tree, if it fails something went wrong while creating the binary tree (such as creating the duplicated value)
-     * or the size count went wrong
-     * The root is assigned with 8
-     * TEST: PASSED
-     */
-    @Test
-    public void tree2(){
-        int[] futureTree = {8,3,10,1,6,14,12,4,3};
-        myBinarySearchTreeNode tree = new myBinarySearchTreeNode(futureTree);
-
-        tree.print();
-
-        assertEquals(8, tree.size());
-    }
-
-    /*
-     * The purpose of this test is to check the correct implementation of the method depth
-     * The array passed includes negative values
-     * It should return the depth of the value passed (edges between the root and the node with the value)
-     * If the test fails it means that something went wrong while creating the binary tree or there must be an error due to counting in the depth method
-     * The root is assigned with 1
-     * TEST: PASSED
-     */
-    @Test
-    public void tree3(){
-        int[] futureTree = {1,0,-3,5,2,3,-1,6};
-        myBinarySearchTreeNode tree = new myBinarySearchTreeNode(futureTree);
-
-        tree.print();
-
-        assertEquals(3, tree.depth(3));
-    }
-
-    /*
-     * The purpose of this test is to check the correct implementation of the method insert
-     * This method checks if the insertion method worked (creating each node where it is supposed to, not creating duplicates)
-     * If it fails, the method could be adding duplicated values or putting the values in the wrong position
-     * The root is assigned with 2
-     * TEST: PASSED
-     */
-    @Test
-    public void tree4(){
         int[] futureTree = {2,1,5};
         myBinarySearchTreeNode tree = new myBinarySearchTreeNode(futureTree);
         tree.insert(5);
@@ -85,18 +31,74 @@ public class myBinarySearchTreeTester {
     }
 
     /*
-     * The purpose of this test is to check  
-     * This method checks if
-     * If it fails,
-     * The root is assigned with
-     * TEST:
-     */
+        * The purpose of this test is to check the correct implementation of the method height
+        * This method calculates the height of the entire subtree and returns the greatest one (height of the entire tree)
+        * If the test fails it means that something went wrong while creating the binary tree or there must be an error due to counting in the height
+        * The root is assigned with 2
+        * TEST: PASSED
+    */
     @Test
-    public void tree5(){
-        int[] futureTree = {2,1,5};
+    public void tree2(){
+        int[] futureTree = {2,4,1,3,6,7,5};
         myBinarySearchTreeNode tree = new myBinarySearchTreeNode(futureTree);
 
         tree.print();
+
+        assertEquals(3, tree.height());
+    }
+
+    /*
+         * The purpose of this test is to check the correct implementation of the method size
+         * The array passed contains a duplicated value (it should not be part of the tree)
+         * If the test
+         * It is supposed to calculate the number of nodes in the binary search tree, if it fails something went wrong while creating the binary tree (such as creating the duplicated value)
+         * or the size count went wrong
+         * The root is assigned with 8
+         * TEST: PASSED
+     */
+    @Test
+    public void tree3(){
+        int[] futureTree = {8,3,10,1,6,14,12,4,3};
+        myBinarySearchTreeNode tree = new myBinarySearchTreeNode(futureTree);
+
+        tree.print();
+
+        assertEquals(8, tree.size());
+    }
+
+    /*
+         * The purpose of this test is to check the correct implementation of the method depth
+         * The array passed includes negative values and duplicated values
+         * It should return the depth of the value passed (edges between the root and the node with the value)
+         * If the test fails it means that something went wrong while creating the binary tree or there must be an error due to counting in the depth method
+         * The root is assigned with 1
+         * TEST: PASSED
+     */
+    @Test
+    public void tree4(){
+        int[] futureTree = {1,0,-3,5,2,3,-1,6};
+        myBinarySearchTreeNode tree = new myBinarySearchTreeNode(futureTree);
+
+        tree.print();
+
+        assertEquals(3, tree.depth(3));
+    }
+
+    /*
+         * The purpose of this test is to check the depth of a node value that is not in the binary tree
+         * The array passed has positive integers
+         * If it fails, then it might be the case that the "edges" might be counting and not subtracted after
+         * The root is assigned with 1
+         * TEST: PASSED
+     */
+    @Test
+    public void tree5(){
+        int[] futureTree = {5,4,2,6,8,0,7};
+        myBinarySearchTreeNode tree = new myBinarySearchTreeNode(futureTree);
+
+        tree.print();
+
+        assertEquals(-1,tree.depth(3));
 
     }
 }
